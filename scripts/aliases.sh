feb3e4b040365d613e0a1360215cbdf8537f6f53
hcao <caohongfei@gmail.com>
2015-04-29 22:54:35 +0800

    Support pk and unpk

--- scripts/aliases.sh
+++ scripts/aliases.sh
@@ -1,11 +1,9 @@
-# export BAIDU="/Users/hcao/BaiDuPan/LPSync"
-# export RAR_PREFIX="LearnHome"
 # export SCRIPT_PATH="/Users/hcao/github/playground/scripts"
-# export RAR_PASS=x
 # . ${SCRIPT_PATH}/aliases.sh
 
 export HISTSIZE=99
-export LANG=zh_CN.UTF-8
+export LC_ALL="en_US.UTF-8"
+export LANG="en_US.UTF-8"
 
 alias a='alias'
 alias d1='d 1'
@@ -17,7 +15,7 @@ alias d6='d 6'
 alias d7='d 7'
 alias d8='d 8'
 alias d9='d 9'
-alias ll='ls -lta'
+alias ll='ls -la'
 alias h='history 44'
 alias e1='vi $SCRIPT_PATH/aliases.sh'
 alias e2='vi ~/.bashrc'
@@ -45,29 +43,49 @@ gf() {
     }';
 }
 
-unset -f bk
-bk() {
-  declare -a NAMES
-  NAMES=('BAIDU' 'RAR_PREFIX' 'RAR_PASS')
-  for n in ${NAMES[@]}; do
-    if [[ ${!n} == "" ]]; then echo $n is not defined; sleep 1800; return; fi
+unset -f check_required
+check_required() {
+  declare -a array=("${!1}")
+  for n in ${array[@]}; do
+    if [[ ${!n} == "" ]]; then echo $n is not defined; return 56; fi
   done
-  local name=$(ls -lr $BAIDU/$RAR_PREFIX* | awk '
+}
+
+unset -f pk
+pk() {
+  NAMES=('BAIDU' 'RAR_SOURCE' 'RAR_PREFIX_PK' 'RAR_PASS')
+  if ! check_required NAMES[@]; then return; fi
+  local olddir=$PWD
+  cd $RAR_SOURCE
+  now=$(date +"%Y-%m-%d-%H-%M")
+  file="$RAR_PREFIX_PK$now.rar"
+  echo "Starting back up to $file..."
+  rar a -hp$RAR_PASS -xLearn/00 -xLearn/VOA -xLearn/bpics -xLearn/pics -xLearn/bsnds -xLastFoot/jscripts -xLastFoot/images -xLastFoot/sounds -xLastFoot/videos -xLastFoot/html -xLastFoot/picauds -xLastFoot/TiddlySaver.jar -xLearn/TiddlySaver.jar -x*.DS_Store "$file" Learn LastFoot
+  mv "$file" $BAIDU
+  cd $olddir 
+}
+
+unset -f unpk
+unpk() {
+  NAMES=('BAIDU' 'RAR_PREFIX_UNPK' 'RAR_PASS' 'RAR_TEMP')
+  if ! check_required NAMES[@]; then return; fi
+  local name=$(ls -lr $BAIDU/$RAR_PREFIX_UNPK* | awk '
     {
       if (NR==1) {
         print $9;
       }
     }
   ')
-  local curdir=$( cd "$( dirname "${BASH_SOURCE[1]}" )" && pwd )
-  if [[ $name == "" ]]; then echo No expected file name found; sleep 1800; return; fi
+  local curdir=$RAR_TEMP
+#  local curdir=$( cd "$( dirname "${BASH_SOURCE[1]}" )" && pwd )
+#  if [[ $name == "" ]]; then echo No expected file name found; sleep 1800; return; fi
   echo $curdir
   cp $name $curdir
-  mv $curdir/$RAR_PREFIX*.rar $curdir/$RAR_PREFIX.rar
-  unrar x -p$RAR_PASS $curdir/$RAR_PREFIX.rar Learn LastFoot $curdir/OK/
+  mv $curdir/$RAR_PREFIX_UNPK*.rar $curdir/$RAR_PREFIX_UNPK.rar
+  unrar x -p$RAR_PASS $curdir/$RAR_PREFIX_UNPK.rar Learn LastFoot $curdir/OK/
   /Applications/Beyond\ Compare.app/Contents/MacOS/BCompare "daily"
   rm -r -f $curdir/OK
-  rm $curdir/$RAR_PREFIX.rar
+  rm $curdir/$RAR_PREFIX_UNPK.rar
 }
 
 unset -f d
