a6233194f6960858c9fed75722b28cbde47d8c06
hcao <caohf@genechem.com.cn>
2015-04-29 16:20:50 +0800

    Add directory list support

--- scripts/aliases.sh
+++ scripts/aliases.sh
@@ -8,6 +8,15 @@ export HISTSIZE=99
 export LANG=zh_CN.UTF-8
 
 alias a='alias'
+alias d1='d 1'
+alias d2='d 2'
+alias d3='d 3'
+alias d4='d 4'
+alias d5='d 5'
+alias d6='d 6'
+alias d7='d 7'
+alias d8='d 8'
+alias d9='d 9'
 alias ll='ls -lta'
 alias h='history 44'
 alias e1='vi $SCRIPT_PATH/aliases.sh'
@@ -27,7 +36,7 @@ gf() {
     }
     $0 ~ pattern { 
       if (i == 0) {
-        printf "%s", $1 | "/usr/bin/pbcopy"; print $1, "[31;46m--- copied to clipboard[0m"; 
+        printf "%s", $1 | "/usr/bin/pbcopy"; print $1, "[37;40m--- copied to clipboard[0m"; 
         i ++;
       }
       else {
@@ -43,7 +52,7 @@ bk() {
   for n in ${NAMES[@]}; do
     if [[ ${!n} == "" ]]; then echo $n is not defined; sleep 1800; return; fi
   done
-  local name=$(ls -lt $BAIDU/$RAR_PREFIX* | awk '
+  local name=$(ls -lr $BAIDU/$RAR_PREFIX* | awk '
     {
       if (NR==1) {
         print $9;
@@ -60,3 +69,33 @@ bk() {
   rm -r -f $curdir/OK
   rm $curdir/$RAR_PREFIX.rar
 }
+
+unset -f d
+d() {
+  if [[ $MY_DIRS == "" ]]; then echo No directories defined; return; fi
+  if [[ $1 == "" ]]; then
+    select d in ${MY_DIRS[@]}; do
+      if [[ $d != "" ]]; then cd $d; fi
+      break;
+    done
+  else
+    if [[ ! $1 =~ ^[0-9]+$ ]]; then echo Invalid directory number; return; fi
+    local index=$(( $1-1 ))
+    if [[ ${MY_DIRS[$index]} == "" ]]; then echo Specified directory number doesn\'t exist; return; fi
+    cd ${MY_DIRS[$index]}
+  fi
+}
+
+unset -f command_not_found_handle
+#bash 4.0 is required for this
+command_not_found_handle() {
+  if [[ $1 =~ d([0-9]+) ]]; then 
+    echo Redirect to \"d ${BASH_REMATCH[1]}\" 
+    d ${BASH_REMATCH[1]}; 
+  else 
+    echo $1 - command not found
+    return 127
+  fi
+}
+
+
