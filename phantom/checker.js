126e79d15e75a44ff6df9d991d005a55e970184a
hcao <caohongfei@gmail.com>
2015-05-19 22:26:00 +0800

    First operable version of price checker

--- phantom/checker.js
+++ phantom/checker.js
@@ -1,3 +1,29 @@
+//By Hongfei Cao on 5/19/2015
+//This script will discover the changes of the specific part of a specific page on the internet and notify you about it
+//For example, it can detect the price change of a specific product on gnc.com, thus you won't miss a promotion
+
+String.prototype.format = function() {
+    var str = this;
+    var lastArg = arguments[arguments.length - 1];
+    var f = (typeof lastArg === "function") ? lastArg : null;
+    var lastIndexToReplace = (f ? arguments.length - 2 : arguments.length - 1);
+    for (var i = lastIndexToReplace; i >= 0; i --) {
+        var arg = arguments[i];
+        if (typeof arg === "object")
+            Object.keys(arg).forEach(function(para) {
+                str = str.replace(para, f ? f(arg[para]) : arg[para]);
+            });
+        else {
+            arg = "" + arg; //toString()
+            str = str.replace("$" + (i + 1), f ? f(arg) : arg);
+        }
+    }
+    return str;
+};
+
+//Repeat execution of a function with 'interval' indefinitely.
+//It differs from setInterval() in that it will schedule next execution only after the current job is completed.
+//Instead, setInterval() will just trigger the execution periodically
 Function.prototype.repeat = function (interval) {
     var __method = this, args = Array.prototype.slice.call(arguments, 1);
     function call() {
@@ -28,6 +54,22 @@ Function.prototype.wait = function (interval, maximum, runAtOnce) {
     }
 };
 
+//A variation from phantomjs' version of wartFor - the onReady() call will receive a parameter about the result of testFx()
+function waitFor(testFx, onReady, timeOutMillis) {
+    var maxtimeOutMillis = timeOutMillis ? timeOutMillis : 3000, //< Default Max Timout is 3s
+        start = new Date().getTime(),
+        condition = false,
+        interval = setInterval(function() {
+            if ( (new Date().getTime() - start < maxtimeOutMillis) && !condition ) {
+                // If not time-out yet and condition not yet fulfilled
+                condition = (typeof(testFx) === "string" ? eval(testFx) : testFx()); //< defensive code
+            } else {
+                clearInterval(interval); //< Stop this interval
+                onReady(condition);      //call the completion callback with the checking result
+            }
+        }, 250); //< repeat check every 250ms
+}
+
 function formatDate(date) {
     function padding(num) {
         return num < 10 ? "0" + num : String(num);
@@ -39,31 +81,49 @@ function formatDate(date) {
 }
 
 var configurations = [
-    //{
-    //    name: "GNC Triple Fish Oil",
-    //    url: "http://www.gnc.com/GNC-Triple-Strength-Fish-Oil-1500/product.jsp?productId=12643679",
-    //    selector: ".product-price.product-sprite .now",
-    //    timeout: 40,    //seconds
-    //    loadImages: false,
-    //    alreadyHasJQ: true,
-    //    useProxy: true,
-    //    interval: 30    //minutes
-    //},
     {
+        identifier: 1,
+        name: "GNC Triple Fish Oil",
+        url: "http://www.gnc.com/GNC-Triple-Strength-Fish-Oil-1500/product.jsp?productId=12643679",
+        selector: ".product-price.product-sprite .now, .product-promos.product-sprite",
+        timeout: 40,    //seconds
+        loadImages: false,
+        alreadyHasJQ: true,
+        useProxy: true,
+        interval: 30    //minutes
+    },
+    {
+        identifier: 2,
         name: "Laoshan Bee",
         url: "http://detail.tmall.com/item.htm?spm=a1z10.1-b.w8171270-9438708950.55.zZhz6k&id=14943725766",
-        selector: ".tm-promo-price .tm-price",
+        selector: ".tm-promo-price .tm-price, .tm-shopPromotion-title.tm-gold",
         timeout: 20,
-        loadImages: true,
+        loadImages: false,
         alreadyHasJQ: false,
         useProxy: false,
-        interval: 0.1    //minutes
+        interval: 30    //minutes
     }
 ];
 
 var tasks = [];
 
 var records = [];
+
+function addRecord(result) {
+    records.push(result);
+    var lastRecord;
+    for (var i = records.length - 2; i >= 0; i--) {
+        if (records[i].identifier === result.identifier) {
+            lastRecord = records[i];
+            break;
+        }
+    }
+    if (!lastRecord || lastRecord.text !== result.text) {
+        var process = require("child_process");
+        var spawn = process.spawn;
+        spawn("osascript", ["-e", 'tell app "System Events" to display dialog "$1 changed from\n\n$2\n\nto\n\n$3"'.format(result.name, lastRecord ? lastRecord.text : "EMPTY", result.text)]);
+    }
+}
 
 function checkOneConfiguration(task) {
     var config = task.configuration;
@@ -80,6 +140,8 @@ function checkOneConfiguration(task) {
     //time out checking
     var isFinished = false; //success and fail are both "finished"
     var result = {
+        identifier: config.identifier,
+        name: config.name,
         timedOut: false,
         emptyText: false,
         text: ""
@@ -91,36 +153,6 @@ function checkOneConfiguration(task) {
             page.stop();
         }
     }, config.timeout * 1000);
-
-    function analysePage(injectSuccess) {
-        function readInfo() {
-            var text = page.evaluate(function(config) {
-                return jQuery(config.selector).text();
-            }, config);
-            if (!text) {
-                result.emptyText = true;
-                result.text = text;
-                return false;
-            }
-            else {
-                result.emptyText = false;
-                result.text = text;
-                return true;
-            }
-        }
-        if (injectSuccess) {
-            var text = page.evaluate(function(config) {
-                return jQuery(config.selector).text();
-            }, config);
-            if (!text) {
-                result.emptyText = true;
-            }
-            result.text = text;
-        } else {
-            result.text = "Inject failure";
-        }
-        page.render(config.name + ".png");
-    }
 
     //page.onConsoleMessage = function(msg, lineNum, sourceId) {
     //    console.log('CONSOLE: ' + msg + ' (from line #' + lineNum + ' in "' + sourceId + '")');
@@ -139,27 +171,48 @@ function checkOneConfiguration(task) {
     };
 
     page.open(config.url, function(status) {
-        console.log(status);
-        console.log('Content: ' + page.content);
+        //console.log(status);
+        //console.log('Content: ' + page.content);
         clearTimeout(timeout);
         isFinished = true;
+        var injectSuccess;
         if (!config.alreadyHasJQ) {
-            if (page.injectJs("jquery.min.js")) {
-                analysePage(true);
-            }
-            else {
-                analysePage(false);
-            }
+            injectSuccess = !!page.injectJs("jquery.min.js");
         }
         else {
-            analysePage(true);
+            injectSuccess = true;
         }
-        records.push(result);
-        setTimeout(function() {
-            createTask(config);
-        }, config.interval * 60 * 1000);
-        task.completed = true;
-        console.log((result.text ? result.text : "'EMPTY'") + "\n");
+        if (injectSuccess) {
+            waitFor(function() {
+                return page.evaluate(function(config) {
+                    return !!jQuery(config.selector).text();
+                }, config);
+            }, function(criteriaMet) {
+                if (criteriaMet) {
+                    result.text = page.evaluate(function(config) {
+                        return jQuery(config.selector).text();
+                    }, config);
+                } else {
+                    result.text = "";
+                    result.emptyText = true;
+                }
+                end();
+            }, 20 * 1000);
+        } else {
+            result.text = "Inject failure";
+            end();
+        }
+
+        function end() {
+            result.text = result.text.replace(/[\t]+/g, "").replace(/[\r\n]+/g, "\n").trim();
+            page.render(config.name + ".png");
+            addRecord(result);
+            setTimeout(function() {
+                createTask(config);
+            }, config.interval * 60 * 1000);
+            task.completed = true;
+            console.log((result.text ? result.text : "'EMPTY'") + "\n");
+        }
     });
 }
 
@@ -185,4 +238,4 @@ for (var i = 0; i < configurations.length; i++) {
     createTask(configurations[i]);
 }
 
-scheduleTasks.repeat(500);
+scheduleTasks.repeat(500);  //Pick up a task every 0.5s
