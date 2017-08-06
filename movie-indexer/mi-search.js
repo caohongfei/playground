883e3d06301336d8d9ce96d3c6db05eb3c32972c
hcao <caohongfei@gmail.com>
2017-08-06 22:54:30 +0800

    To find duplicate file in movie-indexer

--- movie-indexer/mi-search.js
+++ movie-indexer/mi-search.js
@@ -3,11 +3,49 @@ if (process.argv.length <= 2) {
     process.exit(0)
 }
 
+const toFindDuplicates = (process.argv[2] === 'XYZ')
+
+// search duplicated files
+function findDuplicates(db) {
+    let sizeToFileName = {}
+    let found = false
+    let threshold = 1000000
+    function search(includes) {
+        Object.keys(includes).forEach(key => {
+            let content = includes[key]
+            if (content.statistics.size < threshold) {
+                return
+            }
+            if (!content.isDirectory) {
+                if (sizeToFileName[content.statistics.size]) {
+                    console.log('Size: ' + numeral(content.statistics.size).format('0,0'))
+                    console.log('    ' + sizeToFileName[content.statistics.size])
+                    console.log('    ' + content.path)
+                    found = true
+                }
+                else {
+                    sizeToFileName[content.statistics.size] = content.path
+                }
+            }
+            else {
+                search(content.includes)
+            }
+        })
+    }
+    for (let i = 0; i < db.length; i++) {
+        search(db[i].includes)
+    }
+    if (!found) {
+        console.log('Nothing found for threshold ' + numeral(threshold).format('0,0'))
+    }
+    process.exit(0)
+}
+
 // search phrases are from index 2 and on
 const terms = process.argv.slice(2).map(s => s.toLowerCase())
 const numeral = require('numeral')
 
-const shared = require("./shared.js")
+const shared = require('./shared.js')
 
 shared.checkDBFileEnv();
 
@@ -23,7 +61,7 @@ function searchOneIncludes(includes, results) {
             })) {
             let line = '    ' + content.path
             if (!content.isDirectory) {
-                line += " (" + numeral(content.statistics.size).format("0,0") + ")"
+                line += ' (' + numeral(content.statistics.size).format('0,0') + ')'
             }
             results.push(line)
         }
@@ -40,15 +78,19 @@ require('load-json-file')(process.env.MI_DB)
         shared.checkDBFile(db)
 
         if (db.length === 0) {
-            console.log("None")
+            console.log('None')
         }
-        for (let i = 0; i < db.length; i++) {
-            let results = []
-            searchOneIncludes(db[i].includes, results)
-            if (results.length > 0) {
-                shared.print(i, db)
-                results.forEach(line => console.log(line))
+        if (!toFindDuplicates) {
+            for (let i = 0; i < db.length; i++) {
+                let results = []
+                searchOneIncludes(db[i].includes, results)
+                if (results.length > 0) {
+                    shared.print(i, db)
+                    results.forEach(line => console.log(line))
+                }
             }
+        } else {
+            findDuplicates(db)
         }
     })
     .catch(err => {
