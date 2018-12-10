dc4fd3aa040e30ebedfccdd59cc2310ad861b441
hcao <caohf@genechem.com.cn>
2018-12-10 11:06:12 +0800

    New TestSwift

---
+++ TestSwift/TestSwift/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  TestSwift
+//
+//  Created by Hongfei Cao on 2018/12/10.
+//  Copyright © 2018 Personal. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
