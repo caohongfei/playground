77a7254ada708d7ecdd7b0407a1c5e44351ae6a7
hcao <caohf@genechem.com.cn>
2021-09-09 18:16:45 +0800

    Vue3 test

---
+++ VueTest/tools/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <p><router-link to="/">Go to Home</router-link></p>
+  <p><router-link to="/about">Go to About</router-link></p>
+  <router-view></router-view>
+  <hr>
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ VueTest/tools/src/components/About.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>About</div>
+    <div><button v-on:click="goToDashboard">Click</button></div>
+</template>
+
+<script>
+    export default {
+        name: 'About',
+        props: {
+            msg: String
+        },
+        methods: {
+            goToDashboard() {
+                console.log(this.$router)
+            }
+        }
+    }
+</script>
