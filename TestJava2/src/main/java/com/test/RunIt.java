03dfbf17c6376785b9c64a52fc3a23606089f8a2
hcao <caohongfei@gmail.com>
2021-12-28 20:06:19 +0800

    immutables

--- TestJava2/src/main/java/com/test/RunIt.java
+++ TestJava2/src/main/java/com/test/RunIt.java
@@ -3,9 +3,12 @@ package com.test;
 import com.google.inject.*;
 import com.google.inject.name.Named;
 import com.google.inject.name.Names;
+import org.immutables.value.Value;
 
 import javax.inject.Qualifier;
 import java.lang.annotation.Retention;
+import java.util.List;
+import java.util.Set;
 
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
@@ -80,5 +83,27 @@ public class RunIt {
 
         // Prints "hello world" 3 times to the console.
         greeter.sayHello();
+
+        test1();
     }
+
+    private static void test1() {
+        FoobarValue value = ImmutableFoobarValue.builder()
+                .foo(2)
+                .bar("Bar")
+                .addBuz(1, 3, 4)
+                .build(); // FoobarValue{foo=2, bar=Bar, buz=[1, 3, 4], crux={}}
+
+        int foo = value.foo(); // 2
+
+        List<Integer> buz = value.buz(); // ImmutableList.of(1, 3, 4)
+    }
+}
+
+@Value.Immutable
+abstract class FoobarValue {
+    public abstract int foo();
+    public abstract String bar();
+    public abstract List<Integer> buz();
+    public abstract Set<Long> crux();
 }
